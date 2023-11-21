35d2600be08b19d7c51124d7379ec83ee9b9005b
Smit56R <82604082+Smit56R@users.noreply.github.com>
2023-11-21 14:35:24 +0530

    Initial commit

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    Transform goal;
+    public float speed = 5f;
+    float accuracy = 1f;
+    float rotSpeed = 2f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[2];
+    }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[0]);
+        currentWP = 0;
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[4]);
+        currentWP = 0;
+    }
+
+    public void GoToContainers()
+    {
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if(g.getPathLength() == 0 || currentWP == g.getPathLength())
+        {
+            return;
+        }
+        currentNode = g.getPathPoint(currentWP);
+
+        if (Vector3.Distance(currentNode.transform.position, transform.position) < accuracy) currentWP++;
+
+        if(currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Pathfinding using A\* in Unity
+
+## Overview
+
+Implementing the A\* algorithm to develop an intelligent character that can find the shortest path through a game environment. Based on the tutorial from Penny de Byl.
